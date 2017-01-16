c2f106bc6f8d211ab21f68a20d937c71127070c4
gschomburg <gschomburg@gmail.com>
2017-01-16 11:20:18 -0500

    working on character snapping

---
+++ cairn_unity/Assets/RotateObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour {
+    public float speed = .1f;
+    //public Vector3 rotation = new Vector3(0,0,0);
+    Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        // transform.localEulerAngles = transform.localEulerAngles + new Vector3(0, speed, 0);
+        //Quaternion deltaRotation = Quaternion.Euler(eulerAngleVelocity * Time.deltaTime);
+        //rb.MoveRotation(rb.rotation * deltaRotation);
+        rb.AddTorque(0, speed, 0);
+    }
+}
