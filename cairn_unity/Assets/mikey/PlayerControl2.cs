f3c000a1fa14da417d06bae4587df1e509152754
Justin Bakse <justin@tsfim.com>
2017-01-21 20:15:54 -0500

    mouse look up and down

--- cairn_unity/Assets/mikey/PlayerControl2.cs
+++ cairn_unity/Assets/mikey/PlayerControl2.cs
@@ -8,17 +8,25 @@ public class PlayerControl2 : MonoBehaviour {
 	public float rotation_speed = 100.0F;
 	public float anim_scale = 100.0f;
 	public float jump_strength = 5.0f;
+	public float look_speed = 100.0f;
+	public Transform neckBone;
+	public Transform torsoBone;
 
+
+	private float headRotationX = 0;
 	Rigidbody rigid_body;
 	Animator animator;
-	// Use this for initialization
+
+
+
+
 	void Start () {
 		animator = GetComponentInChildren<Animator>();
 		rigid_body = GetComponent<Rigidbody>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
+		// handle walk/strafe
 		float forward = Input.GetAxis("Vertical") * forward_speed;
         float left = Input.GetAxis("Horizontal") * strafe_speed;
         forward *= Time.deltaTime;
@@ -26,30 +34,39 @@ public class PlayerControl2 : MonoBehaviour {
         transform.Translate(left, 0, forward);
 
 
+		// handle turn
 		float rotation = Input.GetAxis("RHorizontal") * rotation_speed;
 		rotation *= Time.deltaTime;
 		transform.Rotate(0, rotation, 0);
 
+		// handle head up/down
+		float lookUpDown = Input.GetAxis("RVertical") * look_speed;
+		lookUpDown *= Time.deltaTime;
+		headRotationX += lookUpDown;
+		headRotationX = Mathf.Clamp(headRotationX, -50f, 60f);
+
+		// handle jumping
+		Debug.DrawLine(transform.position + new Vector3(0,.1f,0),
+					   transform.position + new Vector3(0,.1f,0) - Vector3.up * .2f
+					   , Color.red);
 		if (Input.GetButtonDown ("Jump") && IsGrounded(.2f)) {
 			rigid_body.AddForce (new Vector3 (0, jump_strength, 0), ForceMode.Impulse);  
         }
 
-		Debug.DrawLine(transform.position + new Vector3(0,.1f,0),
-					   transform.position + new Vector3(0,.1f,0) - Vector3.up * .2f
-					   , Color.red);
 		 
 		
-
+		// update animation states
 		animator.SetFloat("Speed", forward * anim_scale, 0.04f, Time.deltaTime);
 		animator.SetFloat("Strafe", left * anim_scale, 0.04f, Time.deltaTime);
 		animator.SetBool("Grounded",  IsGrounded(.2f));
 
 	}
-
-	  bool IsGrounded(float checkDistance) {
-		
-       
-	   
+	void LateUpdate(){
+		neckBone.localEulerAngles = new Vector3(-headRotationX * .5f, 180, 0);
+		torsoBone.localEulerAngles = new Vector3(90 + headRotationX * .5f, 0, 0);
+	}
+	
+	 bool IsGrounded(float checkDistance) {   
 		if (Physics.Raycast(transform.position + new Vector3(0,.1f,0), -Vector3.up, checkDistance))
         {
             return true;
